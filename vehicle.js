f68c66b5fc958d5e81f220f30f1926a3d8888185
Brenda <amaya.brendaa@gmail.com>
2023-03-08 17:46:27 -0800

    changes

--- vehicle.js
+++ vehicle.js
@@ -89,3 +89,60 @@ class Vehicle {
 module.exports = {
     Vehicle
 }
+
+
+class Car extends Vehicle{
+    constructor(make, model, year, color, mileage){
+    super(make, model, year, color, mileage)
+        this.maxPassengers = 5
+        this.passenger = 0
+        this.numberOfWheels = 4
+        this.maxSpeed = 160
+        this.fuel = 10
+        this.scheduleService = false
+    }
+
+    checkService(){
+        if (this.mileage > 30000){
+            this.scheduleService = true
+            return this.scheduleService
+        }
+    }
+    start(){
+        if (this.fuel > 0) {
+            console.log("engine has started")
+            return this.started = true;
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassanger(num){
+        if (this.passenger < this.maxPassengers){
+            if (num + this.passenger <= this.maxPassengers){
+                this.passenger = num
+                return this.passenger
+            }
+            else{
+                console.log(`${this.model} ${this.make} does not have enough space to fit all passengers.`)
+            }
+        }
+        else{
+            console.log(`${this.model} ${this.make} cannot fit all passengers`)
+        }
+    }
+}
+
+
+
+
+let myCar = new Car('HONDA', 'ACCORD', '2017', 'WHITE', 90000)
+
+myCar.start()
+myCar.loadPassanger(2)
+myCar.stop()
+myCar.checkService()
+console.log(myCar.scheduleService)
+
+console.log(myCar)
